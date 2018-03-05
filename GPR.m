ca8d657b7b7a9f1f2b611140fed01c85a877e214
Sathish <31508223+Theesh76@users.noreply.github.com>
2018-03-05 10:00:23 +0100

    Delete GPR.m

--- GPR.m
+++
@@ -1,9 +0,0 @@
-gprMdl = fitrgp(tau(:,1),q_m(:,1),'Basis','pureQuadratic','FitMethod','exact','PredictMethod','exact');
-ypred = resubPredict(gprMdl);
-plot(tau(:,1),q_m(:,1),'b.');
-hold on;
-plot(tau(:,1),ypred,'r','LineWidth',1.5);
-xlabel('x');
-ylabel('y');
-legend('Data','GPR predictions');
-hold off
